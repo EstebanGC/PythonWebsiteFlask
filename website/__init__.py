364598e29158b6e85439bcded0dabc90a8978711
Esteban Carvajal <johan.gal.car@gmail.com>
2022-06-30 23:51:27 -0500

    Created login and signup pages

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'newtonwasthesmartesthuman'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,5 +19,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
     return app
+
+def create_database(app):
+    if not path.exists('website/ ' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
